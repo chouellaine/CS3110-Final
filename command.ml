28f2977576ca75efe5a3742fea2cf181d6ec13a4
Ellaine Chou <ec695@cornell.edu>
2019-04-15 13:55:09 -0400

    update doc for main and command

--- command.ml
+++ command.ml
@@ -1,39 +1,3 @@
-(*  Note: 
-     Menu Level 1 accepts commands: "Start", "Quit" 
-     Display:  "Welcome to blah, type Start/Quit, etc"
-
-     Menu Level 2 accepts commands: "Player" , "AI", "Quit"
-     Display: "Player vs Player or Player vs AI,etc "
-
-     Menu Level 3 accepts commands: "Quit", "move","jump","offer draw",
-     "accept draw","reject draw," "score"
-     Display: the board game, Your commands are "move" ,"jump","offer draw", etc
-
-     Commands: "Quit", "Start","Player", "AI", "jump (x1,y1) to (x2,y2)", 
-     "move (x1,y1) to (x2,y2)", "offer draw", "accept draw"," reject draw"
-
-     DISCUSS: are we allowing users to ask for valid moves? 
-
-     TODO:  
-     check command validity: 
-    - check if player jumps over all possible pieces 
-    - check if desired destination is empty 
-    - check if piece at can be crowned
-    - if not a king, check if piece is moving diagonally (left/right) forward one spot 
-       AND if there are no available forward jumps to make 
-    - if king, check if piece is moving diagonally (left/right) forward/backward one spot
-       AND if there are no avialble forward/backward jumps to make 
-    - "move" command can also be interpreted as a "jump" command, but 
-       "jump" command MUST only be jumping over opponent's pieces. 
-    - end game after 50 moves
-    - if player vs player, and one person  "offer draw," other player must either 
-       "accept draw" or "reject draw"
-    - if player vs AI, and player "offer draw", AI always accepts draw (discuss with team)
-
-     Update move history (number of moves, score?) <-- potentially needed for AI? 
-     Tell state to print the updated board state 
-*) 
-
 type action = (int*int) list 
 type ptype = Player | AI 
 
@@ -121,5 +85,3 @@ let parse str =
   let lst = List.rev (parse_rec split []) in 
   if lst = [] then raise Empty 
   else read_cmd lst 
-
-let parse_thunk () = parse(read_line())
