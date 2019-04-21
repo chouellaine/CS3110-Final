a56b83fc5b1c16d091fe9dd25c15a00a33684332
agl62 <agl62@cornell.edu>
2019-04-21 16:58:44 -0400

    added spectator! (I think)

--- Sockets.ml
+++ Sockets.ml
@@ -5,6 +5,20 @@ open Pervasives
 
 type os = Apple | Other
 
+let sto64b s = 
+  let b = Bytes.make 64 (String.get " " 0) in
+  Bytes.blit_string s 0 b 0 (String.length s);
+  b
+
+let rec write_children f_list s =
+  match f_list with 
+  | [] -> ()
+  | h::t -> begin match write (snd h) (sto64b s) 0 64 with
+      | 64 -> ()
+      | _ -> failwith "64 bytes should have been sent"
+    end;
+    write_children t s
+
 let find_port fd = 
   begin
     match getsockname fd with
@@ -25,8 +39,13 @@ let receive fd =
   match recv fd msg 0 64 [] with
   | len -> Bytes.of_string (String.sub (Bytes.to_string msg) 0 len)
 
+let spec_receive fd = 
+  let msg = Bytes.create 64 in
+  match recv fd msg 0 64 [] with
+  | len -> (Bytes.of_string (String.sub (Bytes.to_string msg) 0 len))
+
 let listen_accept fd =
-  listen fd 1;
+  listen fd 5;
   print_string "Please give your IP Address and this port number to your opponent: \n";
   print_endline "Your IP Address: ";
   let code = 
@@ -40,7 +59,7 @@ let listen_accept fd =
   print_endline "Waiting for opponent to connect...";
   accept fd
 
-let conn fd = 
+let conn_client fd = 
   print_string "Please enter your opponent's IP Address:\n";
   let ip = read_line () in
   print_string "Please enter the port number to connect to on your opponents machine:\n";
@@ -48,15 +67,21 @@ let conn fd =
   let conn_addr = ADDR_INET(inet_addr_of_string ip,int_of_string port) in
   (print_board (new_game ()).pieces; Pervasives.print_newline (); connect fd conn_addr)
 
-let rec init_spectators num rfd =
+let conn_spec fd = 
+  print_string "Please enter the IP Address of the host of the game you want to connect to:\n";
+  let ip = read_line () in
+  print_string "Please enter the port number to connect to:\n";
+  let port = read_line () in
+  let conn_addr = ADDR_INET(inet_addr_of_string ip,int_of_string port) in
+  connect fd conn_addr; print_board (new_game ()).pieces; Pervasives.print_newline ()
+
+let init_spectators fd num =
   let rec fork_list num acc = 
     if num = 0 then acc else
       (pipe ())::(fork_list (num-1) acc)
   in
   let lst = fork_list num [] in
-  let fork_accept iofd = 
-    let fd = socket PF_INET SOCK_STREAM 0 in
-    listen fd 1;
+  let fork_accept fd iofd = 
     let conn_fd, conn_sock = accept fd in
     while true do
       let bs = Bytes.create 64 in
@@ -66,12 +91,12 @@ let rec init_spectators num rfd =
       else ()
     done
   in
-  let rec fork_pipes lst =
+  let rec fork_pipes fd lst =
     match lst with
     | [] -> ()
     | (inp,out)::t ->  match fork () with
-      | 0 -> close out; fork_accept (inp,out);
-      | pid -> close inp; ()
+      | 0 -> close out; fork_accept fd (inp,out)
+      | pid -> close inp; fork_pipes fd t
   in
-  fork_pipes lst;
+  fork_pipes fd lst;
   lst
--- Sockets.mli
+++ Sockets.mli
@@ -1,7 +1,17 @@
 type os = Apple | Other
 
+val sto64b : string -> bytes
+
 val receive : Unix.file_descr -> bytes
+
+val spec_receive : Unix.file_descr -> bytes
 
 val listen_accept : Unix.file_descr -> Unix.file_descr * Unix.sockaddr
 
-val conn : Unix.file_descr -> unit
+val conn_client : Unix.file_descr -> unit
+
+val conn_spec : Unix.file_descr -> unit
+
+val init_spectators : Unix.file_descr -> int -> (Unix.file_descr * Unix.file_descr) list
+
+val write_children : (Unix.file_descr * Unix.file_descr) list -> string -> unit
